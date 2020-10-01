62ae555fa0a56570c1814767ca54027eb54331ba
Rogerlais Andrade <rogerlais@gmail.com>
2020-09-30 23:55:08 -0300

    entrega 20200930

--- Lista03.EAD/sc02-03.sh
+++ Lista03.EAD/sc02-03.sh
@@ -2,13 +2,27 @@
 
 # 2 - Escreva um script que peça para o usuário digitar 3 nomes de arquivo e imprima o nome daquele que possui o maior número de linhas.
 
-max_file=""
+max_idx=0
+max_lines=0
 
 read -p "digite nome arquivo(1) : " if1
 read -p "digite nome arquivo(1) : " if2
 read -p "digite nome arquivo(1) : " if3
 
+declare -a StringArray=( "./Lista03.EAD/sc02-03.sh" ${if1} ${if2} ${if3} )
 
-for filename in { ${if1}, ${if2}}; do
-    echo ${filename}
+idx=0
+for currname in ${StringArray[@]}; do
+    ((idx++))
+    echo ${currname}
+    echo ${idx}
+    if test -f "${currname}" ; then
+        currlines=$(wc -l "${currname}")
+    else
+        currlines=0
+    fi
+    if  "${currlines}" -gt "${max_lines}" ; then
+        max_lines = ${currlines}
+        max_idx=${idx}
+    fi
 done
