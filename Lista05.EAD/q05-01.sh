a5e0b33a57359d664e46f77a2b88ba512db49aab
Roger <rogerlais@gmail.com>
2020-11-26 12:46:22 -0300

    to work now

--- Lista05.EAD/q05-01.sh
+++ Lista05.EAD/q05-01.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-<< 'DESC'
+<<'DESC'
 Escreva um script que imprima todos os números pares de 1 até 21. 
 Melhore o script para que imprima todos os números pares entre a e b, sendo a o primeiro parâmetro de linha de comando, enquanto que b é o segundo.
 DESC
@@ -9,6 +9,11 @@ DESC
 export DBG_ENV="DBG"
 if [[ -v "${DBG_ENV}" ]]; then
     #!Ajuste de depuracao! erro se linha não nula não for inserida abaixo
+    echo 'Debug mode !' &>/dev/null
 fi
+
+for ((i = 2; i < 21; i += 2)); do
+    echo "${i}"
+done
 
 echo 'FIM'
--- _templates/qnn-nn.sh
+++ _templates/qnn-nn.sh
@@ -8,6 +8,7 @@ DESC
 export DBG_ENV="DBG"
 if [[ -v "${DBG_ENV}" ]]; then
     #!Ajuste de depuracao! erro se linha não nula não for inserida abaixo
+    echo "debug mode!" > /dev/null
 fi
 
 echo 'FIM'
