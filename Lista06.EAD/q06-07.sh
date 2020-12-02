71f1c2c6c73d2e109b6d81632b3660072d5710cf
Roger <rogerlais@gmail.com>
2020-12-02 19:59:33 -0300

    commit final lista 06

--- Lista06.EAD/q06-07.sh
+++ Lista06.EAD/q06-07.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-<< 'DESC'
+<<'DESC'
 Escreva um script que exiba o arquivo mais recente (cuja criação ou modificação foi realizada mais tarde) do diretório atual.
 DESC
 
@@ -8,34 +8,25 @@ DESC
 export DBG_ENV="DBG"
 if [[ -v "${DBG_ENV}" ]]; then
     #!Ajuste de depuracao! erro se linha não nula não for inserida abaixo
-    echo "debug mode!" > /dev/null
+    echo "debug mode!" >/dev/null
     targetDir=${PWD}/Lista06.EAD
 else
     targetDir=${PWD}
 fi
 clear
 
-
 #ponto de entrada(main)
 strList=""
 for currFile in $targetDir/*; do
     if [ -f "${currFile}" ] && [ -r "${currFile}" ]; then
         currTime="$(stat -c %Y "${currFile}")"
-        #line="$(printf "%012d\t%s" ${currTime} ${currFile})\n" #\\ como separador
         line="$(printf "%05d" ${currTime})\\${currFile}\n" #\\ como separador
-        echo -e "${line}"
         strList+="${line}"
     fi
 done
-echo -e ${strList}
 sortedStrList=$(echo -e "${strList}" | sort -r)
 sortedStrList=$(echo -e "${sortedStrList}" | tr '\\' '\n') #Troca separador anterior por \n
-echo -e ${sortedStrList}
-IFS=$'\n' read -d '' -r -a items <<<${sortedStrList}
-#IFS=$'\n' read -a items <<<"${sortedStrList}"
-
-echo -e "${items[@]}"
-
-#echo -e "$(echo "${items[0]" | cut -f2 -d$'\t')"
+IFS=$'\n' read -d '' -r -a items <<<${sortedStrList}  #usar apenas quebra de linha como separador
+echo -e "Arquivo mais recente: ${items[1]}"
 
 echo 'FIM'
