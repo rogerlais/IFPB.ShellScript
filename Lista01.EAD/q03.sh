5e203f9a4cbf426bbef84c8883ae5e13887d0210
Roger <rogerlais@gmail.com>
2020-12-15 06:59:51 -0300

    faxina

--- Lista01.EAD/q03.sh
+++ Lista01.EAD/q03.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+clear
 i=1
-for dir in "$@"
-do
-	echo "listando diretório($i): $dir"
-	ls -la $dir
-  i=$((i + 1))
-  if (( $i >= 4 ))
-  then
-	  break
-  fi  
+for dir in "$@"; do
+    echo "listando diretório(${i}): ${dir}"
+    ls -la "${dir}"
+    i=$((i + 1))
+    if ((i >= 4)); then
+        break
+    fi
 done
 echo "fim"
--- _templates/qnn-nn.sh
+++ _templates/qnn-nn.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-<< 'DESC'
+echo '{
 comments...
-DESC
+}' >/dev/null
 
 #dados forcados
-export DBG_ENV=$(test "${DBG^^}" == "'YES'" && echo "1" || echo "0")
+if [[ ${DBG^^} == "'YES'" ]]; then #depurador avalia '[[ 'YES' == YES ]]' como verdadeiro
+    DBG_ENV=true
+else
+    DBG_ENV=false
+fi
+export DBG_ENV
 
-if ((${DBG_ENV})); then
+if ${DBG_ENV}; then
     #!Ajuste de depuracao! erro se linha não nula não for inserida abaixo
-    echo "debug mode!" > /dev/null
+    echo "debug mode!" >/dev/null
 fi
 clear
-
 
 #ponto de entrada(main)
 
